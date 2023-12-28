ea18703eeef3f4912fab34b33509c590a41ad30d
egasetiawan <ega.rizky.setiawan@mail.ugm.ac.id>
2023-12-28 12:05:13 +0700

    cors option

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ const connectToMongoDB = require('./lib/db');
 const productRouter = require('./router/productRouter');
 
 const app = express();
-
-const corsOptions = {
-  origin: 'http://localhost:3000', // Replace with your allowed frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: false,
-};
+;
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/product', productRouter);
 
 const PORT = process.env.PORT || 3000; // Set a default port or use an environment variable for production

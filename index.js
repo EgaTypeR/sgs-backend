a99461fa29a0aa5ceaeb65c357be7c30a35bfb0f
egasetiawan <ega.rizky.setiawan@mail.ugm.ac.id>
2023-12-28 11:45:54 +0700

    cors

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 const connectToMongoDB = require('./lib/db');
 const productRouter = require('./router/productRouter')
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your allowed frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: false,
+};
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use('/product',productRouter)
+
 const PORT = process.env.PORT; // Set a port (here, 3000) or use an environment variable for production
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.use(express.json())
-app.use('/product',productRouter)
 
 const db = connectToMongoDB()
 
